30567a603bf23cd756656e3d287aa65cfd10f6bf
Sami Siren <siren@foofactory.fi>
2021-06-08 08:45:13 +0300

    split checks in two parts, make ui url configurable

--- cypress/integration/front-page.ts
+++ cypress/integration/front-page.ts
@@ -3,18 +3,17 @@ import {
   interceptApplicationRounds,
 } from '../fixtures/applicationRound';
 
-import browseAllButton from '../model/home';
-
+const urlPrefix = process.env.UI_URL || 'https://localhost:3000';
 describe('Tilavaraus ui', () => {
   it('Frontpage displays applicationRounds', () => {
     interceptApplicationRounds([applicationRound]);
-    cy.visit('https://localhost:3000');
+    cy.visit(urlPrefix);
     cy.get('h1').should('contain', 'Vakiovuoron hakeminen');
   });
 
   it('Page is accessible', () => {
     interceptApplicationRounds([applicationRound]);
-    cy.visit('https://localhost:3000');
+    cy.visit(urlPrefix);
     cy.a11yCheck();
   });
 });
