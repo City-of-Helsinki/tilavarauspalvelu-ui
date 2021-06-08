9539ee618846464bd66b122a4bce3eaf8d46102f
Sami Siren <siren@foofactory.fi>
2021-06-08 09:23:21 +0300

    tweak env

--- cypress/integration/front-page.ts
+++ cypress/integration/front-page.ts
@@ -3,19 +3,18 @@ import {
   interceptApplicationRounds,
 } from '../fixtures/applicationRound';
 
-const urlPrefix = process.env.UI_URL || 'https://localhost:3000';
 
-console.log('Using url prefix', urlPrefix)
+
 describe('Tilavaraus ui', () => {
   it('Frontpage displays applicationRounds', () => {
     interceptApplicationRounds([applicationRound]);
-    cy.visit(urlPrefix);
+    cy.visit('/');
     cy.get('h1').should('contain', 'Vakiovuoron hakeminen');
   });
 
   it('Page is accessible', () => {
     interceptApplicationRounds([applicationRound]);
-    cy.visit(urlPrefix);
+    cy.visit('/');
     cy.a11yCheck();
   });
 });
