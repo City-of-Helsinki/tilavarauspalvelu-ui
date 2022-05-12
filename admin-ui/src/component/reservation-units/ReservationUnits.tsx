6188fe0c3ff98aa6987b77da2e8e5ed51c5d9ae8
Sami Siren <siren@foofactory.fi>
2022-05-12 13:02:02 +0300

    breadcrumb

--- admin-ui/src/component/reservation-units/ReservationUnits.tsx
+++ admin-ui/src/component/reservation-units/ReservationUnits.tsx
@@ -6,11 +6,13 @@ import { H1 } from "../../styles/new-typography";
 import withMainMenu from "../withMainMenu";
 import SearchForm, { FilterArguments, emptyState } from "./Filters";
 import ReservationUnitsDataReader, { Sort } from "./ReservationUnitsDataLoader";
+import BreadcrumbWrapper from "../BreadcrumbWrapper";
 
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   gap: var(--spacing-layout-2-xs);
+
   padding: var(--spacing-layout-2-xs) 0 var(--spacing-layout-m)
     var(--spacing-layout-m);
   max-width: var(--container-width-l);
@@ -37,20 +39,23 @@ const ReservationUnitsSearch = (): JSX.Element => {
   };
 
   return (
-    <Wrapper>
-      <div>
-        <H1>{t("ReservationUnits.reservationUnitListHeading")}</H1>
-        <p>{t("ReservationUnits.reservationUnitListDescription")}</p>
-      </div>
-      <SearchForm onSearch={debouncedSearch} />
-      <HR />
-      <ReservationUnitsDataReader
-        key={JSON.stringify({ ...search, ...sort })}
-        filters={search}
-        sort={sort}
-        sortChanged={onSortChanged}
-      />
-    </Wrapper>
+    <>
+      <BreadcrumbWrapper route={["spaces-n-settings", "reservation-units"]} />
+      <Wrapper>
+        <div>
+          <H1>{t("ReservationUnits.reservationUnitListHeading")}</H1>
+          <p>{t("ReservationUnits.reservationUnitListDescription")}</p>
+        </div>
+        <SearchForm onSearch={debouncedSearch} />
+        <HR />
+        <ReservationUnitsDataReader
+          key={JSON.stringify({ ...search, ...sort })}
+          filters={search}
+          sort={sort}
+          sortChanged={onSortChanged}
+        />
+      </Wrapper>
+    </>
   );
 };
 
