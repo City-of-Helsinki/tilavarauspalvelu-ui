a7500f9e8c8f911b29d3e9dff804392e09fcb297
Sami Siren <siren@foofactory.fi>
2022-08-25 16:22:28 +0300

    add new view for "all" reservations

--- admin-ui/src/i18n/messages.ts
+++ admin-ui/src/i18n/messages.ts
@@ -231,7 +231,8 @@ const translations: ITranslations = {
   },
   MainMenu: {
     reservations: ["Varaukset"],
-    requestedReservations: ["Alustavat varaukset"],
+    requestedReservations: ["Varaustoiveet"],
+    allReservations: ["Kaikki varaukset"],
     recurringReservations: ["Kausivaraaminen"],
     applicatioNROunds: ["Hakukierrokset"],
     clients: ["Asiakkaat"],
@@ -1279,6 +1280,10 @@ const translations: ITranslations = {
     ],
     reservationListDescription: [
       "Alla näet kaikki käsiteltävät varaustoiveet.",
+    ],
+    allReservationListHeading: ["Kaikki varaukset"],
+    allReservationListDescription: [
+      "Alla näet kaikki tulevat varaukset ja käsittelemättömät varaustoiveet. Voit hakea tai suodattaa varauksia. Klikkaa varauksen nimeä nähdäksesi tarkemmat tiedot varauksesta.",
     ],
     headings: {
       id: ["id"],
