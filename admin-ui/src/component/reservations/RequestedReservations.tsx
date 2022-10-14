7694e2f13af4832299ba908367077cafc3bc05f3
Sami Siren <siren@foofactory.fi>
2022-10-14 12:57:38 +0300

    container/layout fixes for mobile

--- admin-ui/src/component/reservations/RequestedReservations.tsx
+++ admin-ui/src/component/reservations/RequestedReservations.tsx
@@ -1,23 +1,13 @@
 import { debounce } from "lodash";
 import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
-import styled from "styled-components";
 import { H1 } from "common/src/common/typography";
 import withMainMenu from "../withMainMenu";
 import Filters, { FilterArguments, emptyState } from "./Filters";
 import ReservationUnitsDataReader, { Sort } from "./ReservationsDataLoader";
 import BreadcrumbWrapper from "../BreadcrumbWrapper";
 import { HR } from "../lists/components";
-
-const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: var(--spacing-layout-2-xs);
-
-  padding: var(--spacing-layout-2-xs) 0 var(--spacing-layout-m)
-    var(--spacing-layout-m);
-  max-width: var(--container-width-l);
-`;
+import { Container } from "../../styles/layout";
 
 const Reservations = (): JSX.Element => {
   const [search, setSearch] = useState<FilterArguments>(emptyState);
@@ -36,7 +26,7 @@ const Reservations = (): JSX.Element => {
   return (
     <>
       <BreadcrumbWrapper route={["reservations", "requested-reservations"]} />
-      <Wrapper>
+      <Container>
         <div>
           <H1>{t("Reservations.reservationListHeading")}</H1>
           <p>{t("Reservations.reservationListDescription")}</p>
@@ -53,7 +43,7 @@ const Reservations = (): JSX.Element => {
           sort={sort}
           sortChanged={onSortChanged}
         />
-      </Wrapper>
+      </Container>
     </>
   );
 };
