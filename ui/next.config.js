4a9250731afa7c9414f713a3d43bf45e225fd947
huru-ukko <t.pesola@gmail.com>
2023-02-01 14:17:38 +0200

    Updated next.config and environment variable names

--- ui/next.config.js
+++ ui/next.config.js
@@ -11,43 +11,28 @@ const nextConfig = {
   serverRuntimeConfig: {
     apiBaseUrl: process.env.TILAVARAUS_API_URL,
     authEnabled: process.env.DISABLE_AUTH !== "true",
-
-    oidcClientId: process.env.NEXT_PUBLIC_OIDC_CLIENT_ID,
-    oidcClientSecret: process.env.NEXT_PUBLIC_OIDC_AUTH_SECRET,
-    oidcIssuer: process.env.NEXT_PUBLIC_OIDC_URL,
-    oidcTokenUrl: process.env.NEXT_PUBLIC_OIDC_TOKEN_URL,
-    oidcAccessTokenUrl: process.env.NEXT_PUBLIC_OIDC_ACCESS_TOKEN_URL,
-    oidcProfileApiUrl: process.env.NEXT_PUBLIC_OIDC_PROFILE_API_URL,
-    oidcTilavarausApiUrl: process.env.NEXT_PUBLIC_OIDC_TILAVARAUS_API_URL,
-    oidcScope: process.env.NEXT_PUBLIC_OIDC_SCOPE,
-    oidcCallbackUrl: process.env.NEXT_PUBLIC_OIDC_CALLBACK_URL,
+    oidcClientId: process.env.OIDC_CLIENT_ID,
+    oidcClientSecret: process.env.OIDC_CLIENT_SECRET,
+    oidcIssuer: process.env.OIDC_URL,
+    oidcTokenUrl: process.env.OIDC_TOKEN_URL,
+    oidcAccessTokenUrl: process.env.OIDC_ACCESS_TOKEN_URL,
+    oidcScope: process.env.OIDC_SCOPE,
+    oidcCallbackUrl: process.env.OIDC_CALLBACK_URL,
+    oidcProfileApiUrl: process.env.OIDC_PROFILE_API_SCOPE,
+    oidcTilavarausApiUrl: process.env.OIDC_TILAVARAUS_API_SCOPE,
   },
   publicRuntimeConfig: {
     baseUrl: process.env.NEXT_PUBLIC_BASE_URL,
     apiBaseUrl: process.env.TILAVARAUS_API_URL,
     authEnabled: process.env.DISABLE_AUTH !== "true",
-
     oidcEndSessionUrl: process.env.NEXT_PUBLIC_OIDC_END_SESSION,
-
     sentryDSN: process.env.NEXT_PUBLIC_SENTRY_DSN,
     sentryEnvironment: process.env.NEXT_PUBLIC_SENTRY_ENVIRONMENT,
-
     cookiehubEnabled: process.env.NEXT_PUBLIC_COOKIEHUB_ENABLED === "true",
     matomoEnabled: process.env.NEXT_PUBLIC_MATOMO_ENABLED === "true",
     hotjarEnabled: process.env.NEXT_PUBLIC_HOTJAR_ENABLED === "true",
     mockRequests: process.env.NEXT_PUBLIC_MOCK_REQUESTS === "true",
-
     mapboxToken: process.env.NEXT_PUBLIC_MAPBOX_TOKEN,
-  },
-  transpilePackages: ["common"],
-  compiler: {
-    styledComponents: {
-      ssr: true,
-      displayName: true,
-    },
-  },
-  sentry: {
-    hideSourceMaps: true,
   },
   transpilePackages: ["common"],
   compiler: {
