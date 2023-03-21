8ae092a272a929a18fad2c64d8fd06c8ce6fe986
Joonatan Kuosa <joonatan.kuosa@vincit.com>
2023-03-21 14:21:59 +0200

    Fix: TOS styling

--- admin-ui/src/component/my-units/ShowTOS.tsx
+++ admin-ui/src/component/my-units/ShowTOS.tsx
@@ -1,30 +1,20 @@
 import React from "react";
 import styled from "styled-components";
-import { breakpoints } from "common/src/common/style";
 import { ReservationUnitType } from "common/types/gql-types";
 
 // NOTE This is partial duplicate from ui/application/Preview.tsx
 // see if we can combine them (and other Terms later with parameters)
 // TODO max height? is it 135px or 158px or perhaps a reasonable rem measure?
-// TODO --accent-line-color works in the modal but not in the Recurring form
 const Terms = styled.div`
-  border-top: 8px solid var(--accent-line-color);
+  border-top: 8px solid var(--color-bus);
   margin-top: var(--spacing-m);
   margin-bottom: var(--spacing-m);
-  width: (--container-width-m);
   white-space: break-spaces;
   max-height: 10em;
   overflow-y: scroll;
-  background-color: #f7f7f8;
-  /* TODO what is this var name var(--color-hopea); */
-  padding: var(--spacing-s);
+  background-color: var(--color-silver-light);
+  padding: var(--spacing-m);
 
-  @media (max-width: ${breakpoints.m}) {
-    height: auto;
-    background-color: transparent;
-    padding: 0;
-    overflow-y: none;
-  }
   & > h3 {
     margin-top: 0rem;
   }
@@ -38,7 +28,6 @@ const TOSElement = ({ title, text }: { title: string; text: string }) => (
   </Terms>
 );
 
-// TODO used by recurring also => refactor out of this file and folder
 const ShowTOS = ({
   reservationUnit,
 }: {
@@ -47,7 +36,6 @@ const ShowTOS = ({
   const payTerms = reservationUnit.paymentTerms;
   const priceTerms = reservationUnit.pricingTerms;
   const cancelTerms = reservationUnit.cancellationTerms;
-  // TODO missing terms should not be shown at all (or maybe a different notice not the same component)
   return (
     <div>
       <TOSElement
