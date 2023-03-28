dc15a250563643fd7dce5c6b952fda7dbed7fabc
Joonatan Kuosa <joonatan.kuosa@vincit.com>
2023-03-28 16:54:39 +0300

    Fix: show general terms in TOS also

--- admin-ui/src/component/my-units/ShowTOS.tsx
+++ admin-ui/src/component/my-units/ShowTOS.tsx
@@ -1,6 +1,13 @@
 import React from "react";
 import styled from "styled-components";
-import { ReservationUnitType } from "common/types/gql-types";
+import {
+  QueryTermsOfUseArgs,
+  TermsOfUseType,
+  Query,
+  TermsOfUseTermsOfUseTermsTypeChoices,
+  ReservationUnitType,
+} from "common/types/gql-types";
+import { gql, useQuery } from "@apollo/client";
 
 // NOTE This is partial duplicate from ui/application/Preview.tsx
 // see if we can combine them (and other Terms later with parameters)
@@ -21,6 +28,7 @@ const Terms = styled.div`
 `;
 
 // TODO I'm assuming this doesn't use html or markdown (only plain text) is this correct?
+// use <Sanitize> component if we need it
 const TOSElement = ({ title, text }: { title: string; text: string }) => (
   <Terms>
     <h3>{title}</h3>
@@ -28,38 +36,82 @@ const TOSElement = ({ title, text }: { title: string; text: string }) => (
   </Terms>
 );
 
+const TERMS_OF_USE = gql`
+  query TermsOfUse($termsType: TermsOfUseTermsOfUseTermsTypeChoices) {
+    termsOfUse(termsType: $termsType) {
+      edges {
+        node {
+          pk
+          nameFi
+          nameEn
+          nameSv
+          textFi
+          textEn
+          textSv
+          termsType
+        }
+      }
+    }
+  }
+`;
+
+const useGenericTerms = () => {
+  const { data } = useQuery<Query, QueryTermsOfUseArgs>(TERMS_OF_USE, {
+    variables: {
+      termsType: TermsOfUseTermsOfUseTermsTypeChoices.GenericTerms,
+    },
+  });
+
+  const genericTerms = data?.termsOfUse?.edges
+    ?.map((n) => n?.node)
+    ?.filter((n): n is TermsOfUseType => n != null)
+    .find((n) => n.pk != null && ["generic1"].includes(n.pk));
+
+  return genericTerms;
+};
+
 const ShowTOS = ({
   reservationUnit,
 }: {
   reservationUnit: ReservationUnitType;
 }) => {
+  const serviceTerms = reservationUnit.serviceSpecificTerms;
   const payTerms = reservationUnit.paymentTerms;
   const priceTerms = reservationUnit.pricingTerms;
   const cancelTerms = reservationUnit.cancellationTerms;
+
+  const genericTerms = useGenericTerms();
+
   return (
     <div>
-      <TOSElement
-        title="Palveluehdot"
-        text={reservationUnit.termsOfUseFi ?? ""}
-      />
       {(payTerms?.nameFi || payTerms?.textFi) && (
         <TOSElement
-          title={payTerms?.nameFi ?? "Payment terms title missing"}
+          title={payTerms?.nameFi ?? ""}
           text={payTerms?.textFi ?? ""}
         />
       )}
       {(priceTerms?.nameFi || priceTerms?.textFi) && (
         <TOSElement
-          title={priceTerms?.nameFi ?? "Price terms title missing"}
+          title={priceTerms?.nameFi ?? ""}
           text={priceTerms?.textFi ?? ""}
         />
       )}
       {(cancelTerms?.nameFi || cancelTerms?.textFi) && (
         <TOSElement
-          title={cancelTerms?.nameFi ?? "Cancellation terms missing"}
+          title={cancelTerms?.nameFi ?? ""}
           text={cancelTerms?.textFi ?? ""}
         />
       )}
+      {(serviceTerms?.nameFi || serviceTerms?.textFi) && (
+        <TOSElement
+          title={serviceTerms?.nameFi ?? ""}
+          text={serviceTerms?.textFi ?? ""}
+        />
+      )}
+      <TOSElement
+        title={genericTerms?.nameFi ?? ""}
+        text={genericTerms?.textFi ?? ""}
+      />
     </div>
   );
 };
