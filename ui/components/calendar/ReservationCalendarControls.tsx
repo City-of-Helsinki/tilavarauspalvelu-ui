2d90fd0791fa1e66d7da87f05a2bc8365b850536
Mikko Sartanen <sartanen@gmail.com>
2023-03-31 12:46:36 +0300

    TILA-2214 define last reservation date for datepicker

--- ui/components/calendar/ReservationCalendarControls.tsx
+++ ui/components/calendar/ReservationCalendarControls.tsx
@@ -18,7 +18,7 @@ import {
   IconAngleUp,
   Select,
 } from "hds-react";
-import { trim, trimStart } from "lodash";
+import { maxBy, trim, trimStart } from "lodash";
 import { CalendarEvent } from "common/src/calendar/Calendar";
 import {
   convertHMSToSeconds,
@@ -519,6 +519,11 @@ const ReservationCalendarControls = <T extends Record<string, unknown>>({
     trailingZeros: true,
   });
 
+  const lastOpeningDate = maxBy(
+    reservationUnit.openingHours?.openingTimes,
+    (n) => n.date
+  );
+
   const submitButton = createReservation ? (
     <SubmitButtonWrapper>
       <LoginFragment
@@ -617,6 +622,11 @@ const ReservationCalendarControls = <T extends Record<string, unknown>>({
               label={t("reservationCalendar:startDate")}
               language={i18n.language as Language}
               minDate={new Date()}
+              maxDate={
+                lastOpeningDate?.date
+                  ? new Date(lastOpeningDate.date)
+                  : new Date()
+              }
             />
             <StyledSelect
               key={`startTime-${startTime}`}
