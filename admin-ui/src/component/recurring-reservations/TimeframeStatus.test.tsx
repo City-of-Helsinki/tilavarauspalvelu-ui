4f4f9bd82959d0e0b9ce866524ff050b9f3c1322
Joonatan Kuosa <joonatan.kuosa@gmail.com>
2023-04-19 16:32:27 +0300

    remove double mocks from tests

--- admin-ui/src/component/recurring-reservations/TimeframeStatus.test.tsx
+++ admin-ui/src/component/recurring-reservations/TimeframeStatus.test.tsx
@@ -8,17 +8,6 @@ const pastDate = endOfYesterday().toISOString();
 const today = new Date().toISOString();
 const futureDate = endOfTomorrow().toISOString();
 
-jest.mock("react-i18next", () => ({
-  useTranslation: () => {
-    return {
-      t: (str: string) => str,
-      i18n: {
-        changeLanguage: () => new Promise(() => {}),
-      },
-    };
-  },
-}));
-
 test("Past times ok", async () => {
   const component = render(
     <TimeframeStatus
