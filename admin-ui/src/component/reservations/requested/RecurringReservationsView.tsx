c8ef09a6d3c90bd3523b54159eeb2ead1f71582c
Joonatan Kuosa <joonatan.kuosa@gmail.com>
2023-04-24 09:47:35 +0300

    Fix: refetch recurring reservation after remove

--- admin-ui/src/component/reservations/requested/RecurringReservationsView.tsx
+++ admin-ui/src/component/reservations/requested/RecurringReservationsView.tsx
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
 import {
   Query,
+  QueryReservationByPkArgs,
   ReservationsReservationStateChoices,
   type ReservationType,
 } from "common/types/gql-types";
 import { H6 } from "common/src/common/typography";
 import { useTranslation } from "react-i18next";
-import { useQuery } from "@apollo/client";
+import { useLazyQuery, useQuery } from "@apollo/client";
 import { format } from "date-fns";
-import { RECURRING_RESERVATION_QUERY } from "./queries";
+import { RECURRING_RESERVATION_QUERY, RESERVATION_QUERY } from "./queries";
 import { useNotification } from "../../../context/NotificationContext";
 import ReservationList from "../../ReservationsList";
 import ReservationListButton from "../../ReservationListButton";
@@ -17,28 +18,28 @@ import { useModal } from "../../../context/ModalContext";
 
 const LIMIT = 100;
 
-const RecurringReservationsView = ({
-  reservation,
-  onSelect,
-  onChange,
-}: {
-  reservation: ReservationType;
-  onSelect: (selected: ReservationType) => void;
-  onChange: () => void;
-}) => {
+/// Returns both refetch and refetchSingle
+/// Prefer the use of refetchSingle if at all possible, it takes a reservation pk as an argument
+/// and only updates that.
+/// refetch does a full cache reset that can take a long time and also causes rendering artefacts
+/// because it resets a list to [].
+/// refetchSingle has no error reporting incorrect reservation pk's are ignored
+const useRecurringReservationList = (recurringReservationPk?: number) => {
   const { notifyError } = useNotification();
   const { t } = useTranslation();
+
   const [reservations, setReservations] = useState<ReservationType[]>([]);
 
-  const { loading, refetch } = useQuery<
+  const { loading, refetch: baseRefetch } = useQuery<
     Query,
     { pk: number; offset: number; count: number }
   >(RECURRING_RESERVATION_QUERY, {
-    skip: !reservation.recurringReservation?.pk,
+    skip: !recurringReservationPk,
+    fetchPolicy: "network-only",
     variables: {
-      pk: Number(reservation.recurringReservation?.pk),
-      offset: reservations.length,
+      pk: recurringReservationPk ?? 0,
       count: LIMIT,
+      offset: reservations.length,
     },
     onCompleted: (data) => {
       const qd = data?.reservations;
@@ -56,7 +57,47 @@ const RecurringReservationsView = ({
     },
   });
 
+  const [getReservation] = useLazyQuery<Query, QueryReservationByPkArgs>(
+    RESERVATION_QUERY
+  );
+
+  const refetch = () => {
+    setReservations([]);
+    baseRefetch();
+  };
+
+  const refetchSingle = (pk: number) => {
+    getReservation({ variables: { pk } }).then((res) => {
+      const data = res.data?.reservationByPk;
+      const indexToUpdate = reservations.findIndex((x) => x.pk === data?.pk);
+      if (data && indexToUpdate > -1) {
+        setReservations([
+          ...reservations.slice(0, indexToUpdate),
+          data,
+          ...reservations.slice(indexToUpdate + 1),
+        ]);
+      }
+    });
+  };
+
+  return { loading, reservations, refetch, refetchSingle };
+};
+
+const RecurringReservationsView = ({
+  reservation,
+  onSelect,
+  onChange,
+}: {
+  reservation: ReservationType;
+  onSelect: (selected: ReservationType) => void;
+  onChange: () => void;
+}) => {
+  const { t } = useTranslation();
   const { setModalContent } = useModal();
+
+  const { loading, reservations, refetchSingle } = useRecurringReservationList(
+    reservation.recurringReservation?.pk ?? undefined
+  );
 
   if (loading) {
     return <div>Loading</div>;
@@ -68,9 +109,9 @@ const RecurringReservationsView = ({
     console.warn("Change NOT Implemented.");
   };
 
-  const handleCloseRemoveDialog = (shouldRefetch?: boolean) => {
-    if (shouldRefetch) {
-      refetch();
+  const handleCloseRemoveDialog = (shouldRefetch?: boolean, pk?: number) => {
+    if (shouldRefetch && pk) {
+      refetchSingle(pk);
       onChange();
     }
     setModalContent(null);
@@ -80,7 +121,7 @@ const RecurringReservationsView = ({
     setModalContent(
       <DenyDialog
         reservation={res}
-        onReject={() => handleCloseRemoveDialog(true)}
+        onReject={() => handleCloseRemoveDialog(true, res.pk ?? undefined)}
         onClose={() => handleCloseRemoveDialog(false)}
       />,
       true
