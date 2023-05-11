43e494609c856fb6f0d418974d13032e8876ccff
Joonatan Kuosa <joonatan.kuosa@gmail.com>
2023-05-11 15:58:42 +0300

    Fix type casts

--- admin-ui/src/component/filters/SingleReservationUnitFilter.tsx
+++ admin-ui/src/component/filters/SingleReservationUnitFilter.tsx
@@ -27,8 +27,8 @@ const SingleReservationUnitFilter = ({
   const options = (data?.reservationUnits?.edges || [])
     .map((e) => e?.node)
     .map((reservationUnit) => ({
-      label: reservationUnit?.nameFi as string,
-      value: String(reservationUnit?.pk as number),
+      label: reservationUnit?.nameFi ?? "",
+      value: reservationUnit?.pk ?? "",
     }));
   const valueOption = options.find((o) => o.value === value?.value);
 
