253a21b6425b8bc187472a9c0b070e02f1ddd853
Joonatan Kuosa <joonatan.kuosa@gmail.com>
2023-05-24 15:26:44 +0300

    Fix: mobile overflow in calendar

--- admin-ui/src/component/my-units/ReservationUnitCalendarView.tsx
+++ admin-ui/src/component/my-units/ReservationUnitCalendarView.tsx
@@ -1,17 +1,24 @@
 import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import { addDays, formatISO, startOfDay, subDays } from "date-fns";
+import styled from "styled-components";
 
 import SingleReservationUnitFilter from "../filters/SingleReservationUnitFilter";
 import { Grid, HorisontalFlex, Span6 } from "../../styles/layout";
 import ReservationUnitCalendar from "./ReservationUnitCalendar";
 import WeekNavigation from "./WeekNavigation";
 
+// NOTE something sets the child div to grid which causes overflow on mobile
+const Block = styled.div`
+  && {
+    display: block;
+  }
+`;
+
 type Params = {
   unitId: string;
   reservationUnitId: string;
 };
-
 const ReservationUnitCalendarView = (): JSX.Element => {
   const today = formatISO(startOfDay(new Date()));
 
@@ -35,7 +42,7 @@ const ReservationUnitCalendarView = (): JSX.Element => {
         </Span6>
       </Grid>
       {hasReservationUnitId && (
-        <>
+        <Block>
           <HorisontalFlex style={{ justifyContent: "center" }}>
             <WeekNavigation
               date={begin}
@@ -52,7 +59,7 @@ const ReservationUnitCalendarView = (): JSX.Element => {
             begin={begin}
             reservationUnitPk={reservationUnitId}
           />
-        </>
+        </Block>
       )}
     </>
   );
