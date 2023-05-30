8db29a188599a410075a5e61bf5eac34874641dc
Mikko Sartanen <sartanen@gmail.com>
2023-05-30 11:38:55 +0300

    TILA-2339 cleanup

--- ui/components/common/Toast.tsx
+++ ui/components/common/Toast.tsx
@@ -14,6 +14,7 @@ export const Toast = (props: NotificationProps & { trapFocus?: boolean }) => {
       {...(trapFocus && {
         focusTrapOptions: {
           initialFocus: "#toast-wrapper",
+          clickOutsideDeactivates: true,
         },
       })}
     >
