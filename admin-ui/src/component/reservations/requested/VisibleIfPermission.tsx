dfad2597ff2b6f4ba3377f7241b97e3e6130382f
Joonatan Kuosa <joonatan.kuosa@gmail.com>
2023-06-01 15:08:16 +0300

    Remove unused code

--- admin-ui/src/component/reservations/requested/VisibleIfPermission.tsx
+++ admin-ui/src/component/reservations/requested/VisibleIfPermission.tsx
@@ -4,51 +4,6 @@ import { useQuery } from "@apollo/client";
 import { useAuthState } from "../../../context/AuthStateContext";
 import { CURRENT_USER } from "../../../context/queries";
 
-type Props = {
-  unitPk: number;
-  serviceSectorPks: number[];
-  permissionName: string;
-  children: React.ReactNode;
-  otherwise?: React.ReactNode;
-};
-
-const VisibleIfPermission = ({
-  unitPk,
-  serviceSectorPks,
-  permissionName,
-  children,
-  otherwise,
-}: Props): JSX.Element => {
-  const { hasPermission } = useAuthState().authState;
-  const permission = hasPermission(permissionName, unitPk, serviceSectorPks);
-
-  return (
-    <>
-      {permission && children}
-      {!permission && otherwise}
-    </>
-  );
-};
-
-const VisibleIfPermissionWrapper = (props: {
-  unitPk?: number;
-  serviceSectorPks: number[];
-  permissionName: string;
-  children: React.ReactNode;
-  otherwise?: React.ReactNode;
-}) => {
-  const { unitPk } = props;
-
-  if (!unitPk) {
-    return null;
-  }
-  if (props.serviceSectorPks.length === 0) {
-    return null;
-  }
-
-  return <VisibleIfPermission {...props} unitPk={unitPk} />;
-};
-
 const VisibleIfOwnOrPermission = ({
   reservation,
   permissionName,
