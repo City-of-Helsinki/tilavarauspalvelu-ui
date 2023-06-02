c724769e644c596180ce1dbca09eb8dd15c2b892
Joonatan Kuosa <joonatan.kuosa@gmail.com>
2023-06-02 13:34:16 +0300

    Fix: lint tests

--- admin-ui/src/component/my-units/MyUnitRecurringReservation/MyUnitRecurringReservationForm.test.tsx
+++ admin-ui/src/component/my-units/MyUnitRecurringReservation/MyUnitRecurringReservationForm.test.tsx
@@ -8,8 +8,6 @@ import {
   waitFor,
   within,
 } from "@testing-library/react";
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { expect, test, jest } from "@jest/globals";
 import userEvent from "@testing-library/user-event";
 import { MockedProvider } from "@apollo/client/testing";
 import { getYear, nextMonday, set } from "date-fns";
