3baf62e89ae48604658420382139b6a56c0046ca
Joonatan Kuosa <joonatan.kuosa@gmail.com>
2023-06-20 09:48:40 +0300

    Add: single reservation move in a recurring series

--- admin-ui/src/component/reservations/requested/RequestedReservation.tsx
+++ admin-ui/src/component/reservations/requested/RequestedReservation.tsx
@@ -6,6 +6,7 @@ import { useTranslation } from "react-i18next";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
 import { TFunction } from "i18next";
+import { add, startOfISOWeek } from "date-fns";
 import { breakpoints } from "common/src/common/style";
 import {
   Maybe,
@@ -44,7 +45,7 @@ import VisibleIfPermission from "./VisibleIfPermission";
 import { Accordion } from "../../../common/hds-fork/Accordion";
 import ApprovalButtons from "./ApprovalButtons";
 import RecurringReservationsView from "./RecurringReservationsView";
-import { useRecurringReservations } from "./hooks";
+import { useRecurringReservations, useReservationData } from "./hooks";
 import ApprovalButtonsRecurring from "./ApprovalButtonsRecurring";
 import ReservationTitleSection from "./ReservationTitleSection";
 import { SINGLE_RESERVATION_QUERY } from "./hooks/queries";
@@ -277,19 +278,35 @@ const TimeBlock = ({
   const shownReservation =
     new Date(reservation.begin) > new Date() ? reservation : nextReservation;
 
-  const focusDate =
+  const [focusDate, setFocusDate] = useState<Date>(
     maybeStringToDate(selected?.begin) ??
-    onlyFutureDates(maybeStringToDate(shownReservation?.begin)) ??
-    new Date();
+      onlyFutureDates(maybeStringToDate(shownReservation?.begin)) ??
+      new Date()
+  );
+
+  // No month view so always query the whole week even if a single day is selected
+  // to avoid spamming queries and having to deal with start of day - end of day.
+  // focus day can be in the middle of the week.
+  const { events: eventsAll, refetch: calendarRefetch } = useReservationData(
+    startOfISOWeek(focusDate),
+    add(startOfISOWeek(focusDate), { days: 7 }),
+    String(reservation?.reservationUnits?.[0]?.pk),
+    reservation?.pk ?? undefined
+  );
 
   return (
     <>
       {reservation.recurringReservation?.pk && (
         <Accordion heading={t("RequestedReservation.recurring")}>
+          {/* TODO changes do update the calendar; but what happens if we change an event on another week? */}
           <RecurringReservationsView
             recurringPk={reservation.recurringReservation.pk}
             onSelect={setSelected}
             onReservationUpdated={onReservationUpdated}
+            onChange={() => {
+              onReservationUpdated();
+              calendarRefetch();
+            }}
           />
         </Accordion>
       )}
@@ -299,11 +316,14 @@ const TimeBlock = ({
         id="reservation-calendar"
       >
         <Calendar
-          reservationUnitPk={String(reservation?.reservationUnits?.[0]?.pk)}
           reservation={reservation}
           selected={selected}
           focusDate={focusDate}
-          refetch={onReservationUpdated}
+          refetch={(d) => {
+            onReservationUpdated();
+            setFocusDate(d);
+          }}
+          events={eventsAll}
         />
       </Accordion>
     </>
