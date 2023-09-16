754050b5072d4764affd6fdb71b3e510c4560a3c
Joonatan Kuosa <joonatan.kuosa@gmail.com>
2023-09-16 10:54:41 +0300

    fix: typescript

--- apps/admin-ui/src/component/reservations/requested/DenyDialog.tsx
+++ apps/admin-ui/src/component/reservations/requested/DenyDialog.tsx
@@ -142,7 +142,7 @@ const DialogContent = ({
         fields: {
           // find the pk => slice the array => replace the state variable in the slice
           // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-          // @ts-ignore; TODO: typecheck broke after updating Apollo or Typescript
+          // @ts-expect-error; TODO: typecheck broke after updating Apollo or Typescript
           reservations(existing: ReservationTypeConnection) {
             const queryRes = data?.denyReservation;
             if (queryRes?.errors) {
