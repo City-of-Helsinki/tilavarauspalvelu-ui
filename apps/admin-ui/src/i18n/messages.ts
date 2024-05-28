c8465f2acd74c09357af6d62e95782d884822263
Joonatan Kuosa <joonatan.kuosa@gmail.com>
2024-05-28 11:11:21 +0300

    fix: incorrect enum translation

--- apps/admin-ui/src/i18n/messages.ts
+++ apps/admin-ui/src/i18n/messages.ts
@@ -566,7 +566,7 @@ const translations: ITranslations = {
     HANDLED: ["Hyväksytty"],
     IN_ALLOCATION: ["Käsittelyssä"],
     RESERVED: ["Varattu"],
-    UNALLOCATED: ["Ei jaettu"],
+    UNALLOCATED: ["Vastaanotettu"],
   },
   TimeSlotStatusCell: {
     declined: ["Hylätty"],
